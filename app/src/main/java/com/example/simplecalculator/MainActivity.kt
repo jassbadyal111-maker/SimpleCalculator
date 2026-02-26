9f32abc3f74c1128f5494b29e8353c874d53bc3c
Jass <jassbadyal111@gmail.com>
2026-02-26 10:10:36 +0530

    Remove AppCompat and fix runtime crash

--- app/src/main/java/com/example/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.simplecalculator
 
+import android.app.Activity
 import android.os.Bundle
 import android.widget.*
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
